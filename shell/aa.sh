acdbc9f7de62a24a383ec38ae5608f9e6060fd0b
Ginsengfruit <neogood@outlook.com>
2016-10-19 16:57:44 +0800

    modify aa.sh

--- shell/aa.sh
+++ shell/aa.sh
@@ -3,20 +3,32 @@ dirpom() {
 	curdir=$1
 	cd $curdir
 	for subdir in $curdir/*; do
-		if [ -d $subdir ];then
-			dirpom $subdir
-		elif [ -f $subdir ] && [ "$subdir" = "$curdir/pom.xml" ]; then
+		folderPath=$curdir
+		pname=${folderPath##*/}
+		result=$(echo $subdir | grep "$exString")
+		
+		if [ "$result" != "" ]; then
+			echo "CURENT" $curdir
+			echo "SUB"  $subdir
+			echo "AAAA: " $result
+			echo "include @ do nothing"
+		else
+			if [ -d $subdir ];then
+				dirpom $subdir
+			elif [ -f $subdir ] && [ "$subdir" = "$curdir/pom.xml" ]; then
 				echo "pom: " $subdir
-				echo $direc
-				folderPath=$curdir
-				pname=${folderPath##*/}
-				echo "$pname"
+				#echo $direc
+				#echo "$pname"
 				mvn dependency:tree -DoutputFile="$direc/graphml/$pname.graphml" -DoutputType="graphml"
-		fi
+			
+			fi
+		fi	
+		
 	done
 }
 
 direc=$PWD
+exString="@"
 echo $direc
 dirpom $direc
 
